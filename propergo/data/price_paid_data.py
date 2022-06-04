002a2001eb64f3e0a4c218252910e856bdb23fc0
umbralcalc <umbralcalc@gmail.com>
2022-06-04 08:07:44 +0100

    Added csv file loader

--- propergo/data/price_paid_data.py
+++ propergo/data/price_paid_data.py
@@ -1,2 +1,31 @@
-# Link to csv is here: 
-# http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv
+import os
+from pathlib import Path
+from enum import Enum
+import subprocess
+
+class DataLink(Enum):
+    price_paid_data = (
+        "http://prod.publicdata.landregistry.gov.uk."
+        + "s3-website-eu-west-1.amazonaws.com/pp-complete.csv"
+    )
+
+
+def get_csv_file(link: DataLink):
+    # if the folder doesn't even exist then make it
+    pth = Path("data")
+    pth.mkdir(parents=True, exist_ok=True)
+
+    if not os.path.exists("data/" + Path(link.value).name):
+        bashCommand = "wget " + link.value
+        bashCommand += " -O data/" + Path(link.value).name
+        process = subprocess.Popen(bashCommand.split())
+        output, error = process.communicate()
+
+    # Unzip the relevant files if they are zipped
+    if link.value.split(".")[-1] in ["zip"]:
+        bashCommand = "unzip -o " + link.value
+        process = subprocess.Popen(
+            bashCommand.split(),
+            cwd="data/",
+        )
+        output, error = process.communicate()
